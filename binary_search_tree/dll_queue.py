d14086c5cc2265b36ba53c5505bd60bbbdaaf32c
Noble Obioma <nobioma1@gmail.com>
2019-10-17 15:33:22 +0100

    Copy to BST folder

---
+++ binary_search_tree/dll_queue.py
@@ -0,0 +1,25 @@
+from doubly_linked_list import DoublyLinkedList
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        self.storage = DoublyLinkedList(node=None)
+
+    def enqueue(self, value):
+        value = self.storage.add_to_tail(value)
+        self.size += 1
+
+        return value
+
+    def dequeue(self):
+        if self.storage.head == None:
+            return None
+        value = self.storage.remove_from_head()
+        self.size -= 1
+
+        return value
+
+    def len(self):
+        return self.size
---
+++ binary_search_tree/dll_stack.py
@@ -0,0 +1,25 @@
+from doubly_linked_list import DoublyLinkedList
+
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        self.storage = DoublyLinkedList(node=None)
+
+    def push(self, value):
+        value = self.storage.add_to_tail(value)
+        self.size += 1
+
+        return value
+
+    def pop(self):
+        if self.storage.tail == None:
+            return None
+        value = self.storage.remove_from_tail()
+        self.size -= 1
+
+        return value
+
+    def len(self):
+        return self.size
